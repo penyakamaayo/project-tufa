614f02e7c74ca4d9ee2c67822a5e74425ccb97eb
Tepen <elcabron.penyah@gmail.com>
2020-07-30 19:16:25 +0800

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # ruby encoding: utf-8
-User.create(username: 'mimimi', email: 'mi@mimi.com', password: 'mimimi', admin: true)
+User.create(username: 'admin', email: 'admin@admin.com', password: 'testing', admin: true, otp_secret_key: '188eb28a4f94a4846c1e0979ebabd9d59394cfb3f0f9c535bcbee54b573e07d27feee7fc0ec399a57e3b1968f1813d03aabb297a3f7319368cebb4abee18be6f
+  ')
