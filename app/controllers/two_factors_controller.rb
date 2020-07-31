0c92866bde4c37b2b16edef9032e9ab201295a20
Tepen <elcabron.penyah@gmail.com>
2020-07-31 10:49:53 +0800

    cleanup lines in two_factors controller

--- app/controllers/two_factors_controller.rb
+++ app/controllers/two_factors_controller.rb
@@ -1,7 +1,5 @@
 class TwoFactorsController < ApplicationController
-  # before_action :relogin_user, only: [:create]
   def new
-    # @user = current_user.id
     @save_current_user = current_user.dup
 
     if current_user.otp_module_enabled?
@@ -9,7 +7,6 @@ class TwoFactorsController < ApplicationController
       Rails.logger.info('THIS OTP MODULE IS ENABLED')
       Rails.logger.info('THIS OTP MODULE IS ENABLED')
       Rails.logger.info('THIS OTP MODULE IS ENABLED')
-      # sign_out()
     else
       session[:otp_code] = '123'
       redirect_to users_path
@@ -17,7 +14,6 @@ class TwoFactorsController < ApplicationController
   end
 
   def create
-    # render plain: "hehehe"
     if current_user.authenticate_otp(params[:otp_code_token], drift: 60)
       current_user.otp_module_enabled!
       session[:otp_code] = params[:otp_code_token]
